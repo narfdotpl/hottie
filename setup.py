bd4b82812f26ffc53118bc565b62e9a3fcd5758d
Maciej Konieczny <hello@narf.pl>
2012-11-12 16:49:10 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='hottie',
+    version='pre-0.1.0',
+    description='Code hot-swapping for Python 2.7 classes and functions.',
+    author='Maciej Konieczny',
+    author_email='hello@narf.pl',
+    url='https://github.com/narfdotpl/hottie',
+    py_modules=['hottie'],
+)
